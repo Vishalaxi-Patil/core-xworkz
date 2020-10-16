67d539e0dcfe5110362fd78c53d13507e453c168
Vishalaxi-Patil <72905004+Vishalaxi-Patil@users.noreply.github.com>
2020-10-16 09:48:29 +0530

    My first commit

---
+++ Namaskar2.java
@@ -0,0 +1,13 @@
+class Wish
+{
+  public static void main(String args[])
+  { 
+     System.out.println("Namaskar");
+   }
+}
+class Camera
+{
+   public static void main(String args[])
+   {
+   }
+}
