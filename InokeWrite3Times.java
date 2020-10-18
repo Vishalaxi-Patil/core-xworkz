c182af10c62db72c07070dbb46a337170d40dc77
Vishalaxi-Patil <72905004+Vishalaxi-Patil@users.noreply.github.com>
2020-10-18 20:22:29 +0530

    Invoking write 3 times

---
+++ InokeWrite3Times.java
@@ -0,0 +1,15 @@
+class Phone
+{
+	public static void main(String[] params)
+	{
+		System.out.println("Main");
+		write();
+		write();
+		write();
+	}
+	public static void write()
+	{
+		System.out.println("Write");
+	}
+	
+}
