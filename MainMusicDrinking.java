06abf85a8c30fcc0319dfb4c96b664c904833c73
Vishalaxi-Patil <72905004+Vishalaxi-Patil@users.noreply.github.com>
2020-10-18 20:20:10 +0530

    Main_Method_Drinking

---
+++ MainMusicDrinking.java
@@ -0,0 +1,18 @@
+class Student
+{
+	public static void main(String[] student)
+	{
+		System.out.println("Student name called main");
+		music();
+		drinking();
+	}
+	public static void music()
+	{
+		System.out.println("Student name called as music");
+	}
+	public static void drinking()
+	{
+		System.out.println("Student name called as drinking");
+	}
+		
+}
