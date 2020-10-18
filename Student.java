6d3014aa9ab4ec9a77ede59603adfd1a1493e76e
Vishalaxi-Patil <72905004+Vishalaxi-Patil@users.noreply.github.com>
2020-10-18 20:21:41 +0530

    Invoking five methods

---
+++ Student.java
@@ -0,0 +1,27 @@
+class Student
+{
+	public static void main(String[] param)
+	{
+		System.out.println("usn of student 2KA13CS23");
+		branch();
+		college();
+		name();
+		age();
+	}
+	public static void branch()
+	{
+		System.out.println("branch of student CSE");
+	}
+	public static void college()
+	{
+		System.out.println("college name of student SKSVMACET");
+	}
+	public static void name()
+	{
+		System.out.println("name of the student Vishalaxi");
+	}
+	public static void age()
+	{
+		System.out.println("age of the student 22");
+	}
+}
