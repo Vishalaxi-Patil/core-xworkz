edeaa8257367908c03566fffcb3b0a53c67fb4e3
Vishalaxi-Patil <72905004+Vishalaxi-Patil@users.noreply.github.com>
2020-10-20 21:04:49 +0530

    Welcome

---
+++ Welcome.java
@@ -0,0 +1,13 @@
+public class Welcome
+{
+	public static void main(String[] args)
+	{
+		
+		PrintWelcome("Welcome");
+	}
+	public static void PrintWelcome(String welcome)
+	{
+		System.out.println(welcome);
+	}
+	
+}
