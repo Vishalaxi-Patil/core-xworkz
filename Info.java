f544c35f660d02a2539fd9beb79ed0854cb5fb6c
Vishalaxi-Patil <72905004+Vishalaxi-Patil@users.noreply.github.com>
2020-10-20 21:00:52 +0530

    Email and Programming language

---
+++ Info.java
@@ -0,0 +1,16 @@
+public class Info
+{
+	public static void main(String[] args)
+	{
+		PrintEmail("vishalaxi.xworkz@gmail.com");
+		ProgrammingLanguage("Java");
+	}
+	public static void PrintEmail(String email)
+	{
+		System.out.println("Email=" +email);
+	}
+	public static void ProgrammingLanguage(String pl)
+	{
+		System.out.println("programming languages=" +pl);
+	}
+}
