2f38eb2ff6a54f31a1a0f71ea3bfd6bb0d4f687b
Vishalaxi-Patil <72905004+Vishalaxi-Patil@users.noreply.github.com>
2020-10-20 23:01:58 +0530

    Using primitive and string datatype

---
+++ Datatype.java
@@ -0,0 +1,69 @@
+public class Dataype
+{
+	public static void main(String[] param)
+	{
+		byte eyes=2;
+		  System.out.println(eyes);
+		short NumberOfProgrammingLanguages=256;
+		  System.out.println(NumberOfProgrammingLanguages);
+		int NumberOfPopulationInUs=331589837;
+		   System.out.println(NumberOfPopulationInUs);
+		long NumberOfPhoneUsersInWorld=795000000;
+		   System.out.println(NumberOfPhoneUsersInWorld);
+		float versionOfJava=1.8f;
+             System.out.println(versionOfJava);
+		char grade='A';
+		    System.out.println(grade);
+	}
+}
+		    
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+
