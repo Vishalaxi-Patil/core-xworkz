a951b70f8bcdeb369e2a81d21e27046867850283
Vishalaxi-Patil <72905004+Vishalaxi-Patil@users.noreply.github.com>
2020-10-20 21:09:52 +0530

    Information

---
+++ BloodGroup.java
@@ -0,0 +1,12 @@
+class BloodGroup
+{
+	public static void main(String[] adhar)
+	{
+		final String adharNumber="123567785767";
+		final String phoneNumber="8977685647";
+		final String email="vishalaxi.xworkz@gmail.com";
+		Systemtem.out.println("AdharNumber=" +adharNumber);
+		System.out.println("PhoneNumber=" +phoneNumber);
+		System.out.println("Email=" +email);
+	}
+}
